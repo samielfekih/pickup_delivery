ea0e1b767e2b319c385a4d32d0ee0710ab65e107
Jerome Dockes <jerome@dockes.org>
2016-08-27 17:32:37 +0200

    package for pip install

---
+++ pickup_delivery/__init__.py
@@ -0,0 +1,2 @@
+from .utils import read_data_file
+from .pickup_delivery import initial_solution
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pickup_delivery',
+      packages=['pickup_delivery'],
+      install_requires=[
+          'numpy', 'pandas', 'scipy'
+      ],
+      zip_safe=False)
