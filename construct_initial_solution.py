f149cedda9fe044b693923c549819ff175f15ff0
Jerome Dockes <jerome@dockes.org>
2017-01-31 18:54:15 +0100

    add script for initial solution

---
+++ construct_initial_solution.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import os
+import argparse
+from hashlib import sha1
+import random
+
+from pickup_delivery import initial_solution
+
+
+parser = argparse.ArgumentParser(description='compute an initial solution')
+parser.add_argument(
+    'data_file', type=str, help='csv file containing the problem data')
+parser.add_argument(
+    'out_directory', type=str, help='directory in which to store results')
+
+args = parser.parse_args()
+
+data_file = os.path.abspath(os.path.expanduser(args.data_file))
+out_directory = os.path.abspath(os.path.expanduser(args.out_directory))
+pickup_file = os.path.join(out_directory, u'result_pickups_{0}.csv'.format(
+    sha1(u'{0}'.format(random.random()).encode('utf-8')).hexdigest()))
+delivery_file = os.path.join(
+    out_directory, u'result_deliveries_{0}.csv'.format(
+        sha1(u'{0}'.format(random.random()).encode('utf-8')).hexdigest()))
+
+solution = initial_solution(data_file)
+solution.pickups.to_csv(pickup_file)
+solution.deliveries.to_csv(delivery_file)
+
+print(u'{0} requests satisfied'.format(
+    solution.pickups.notnull().values.sum()))
